33b4234f21a4d1948f0f8fccee90412b346dd59f
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-08-13 22:10:19 +0530

    Create 3276-MinimumNumberOfPushesToTypeWordIi

---
+++ 3276-MinimumNumberOfPushesToTypeWordIi/3276-MinimumNumberOfPushesToTypeWordIi.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/13/2026, 10:10:18 PM
+class Solution {
+ public:
+  // Same as 3014. Minimum Number of Pushes to Type Word I
+  int minimumPushes(string word) {
+    int ans = 0;
+    vector<int> count(26);
+
+    for (const char c : word)
+      ++count[c - 'a'];
+
+    ranges::sort(count, greater<>());
+
+    for (int i = 0; i < 26; ++i)
+      ans += count[i] * (i / 8 + 1);
+
+    return ans;
+  }
+};
