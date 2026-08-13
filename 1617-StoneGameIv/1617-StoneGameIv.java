835951c10f37f44ec108112f7d8ee4eb1cfc18c7
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-08-13 22:12:13 +0530

    Create 1617-StoneGameIv

---
+++ 1617-StoneGameIv/1617-StoneGameIv.java
@@ -0,0 +1,25 @@
+// Last updated: 8/13/2026, 10:12:13 PM
+class Solution {
+    private Boolean[] f;
+
+    public boolean winnerSquareGame(int n) {
+        f = new Boolean[n + 1];
+        return dfs(n);
+    }
+
+    private boolean dfs(int i) {
+        if (i <= 0) {
+            return false;
+        }
+        if (f[i] != null) {
+            return f[i];
+        }
+        int k = (int) Math.sqrt(i);
+        for (int j = 1; j <= k; j++) {
+            if (!dfs(i - j * j)) {
+                return f[i] = true;
+            }
+        }
+        return f[i] = false;
+    }
+}
