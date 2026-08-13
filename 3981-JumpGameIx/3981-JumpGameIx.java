621a5aba6d04694a6da56f823c16048d3167f374
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-08-13 22:08:24 +0530

    Create 3981-JumpGameIx

---
+++ 3981-JumpGameIx/3981-JumpGameIx.java
@@ -0,0 +1,23 @@
+// Last updated: 8/13/2026, 10:08:24 PM
+class Solution {
+    public int[] maxValue(int[] nums) {
+        int n=nums.length;
+        int ans[]=new int[n];
+        int preMax[]=new int[n];
+        preMax[0]=nums[0];
+        for(int i=1;i<n;++i)
+        {
+            preMax[i]=Math.max(preMax[i-1],nums[i]);
+
+
+        }
+        int suffMin=1<<30;
+        for(int i=n-1;i>=0;--i)
+        {
+            ans[i]=preMax[i]>suffMin?ans[i+1]:preMax[i];
+            suffMin=Math.min(suffMin,nums[i]);
+        }
+        return ans;
+        
+    }
+}
