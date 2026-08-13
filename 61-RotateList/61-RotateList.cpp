2d3dbf5c47abcb1f4119eee2570f3403218545ff
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-08-13 22:14:28 +0530

    Create 61-RotateList

---
+++ 61-RotateList/61-RotateList.cpp
@@ -0,0 +1,43 @@
+// Last updated: 8/13/2026, 10:14:28 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k==0)
+            return head;
+        int L=1;
+        ListNode* tail = head;
+
+        while(tail->next != NULL)
+        {
+            tail=tail->next;
+            L++;
+        }
+
+        k=k%L;
+        if(k==0)
+        {
+            return head;
+        }
+        tail->next=head;
+         int remain=L-k;
+         ListNode* newTail=head;
+         for(int c=1;c<remain;c++)
+         {
+            newTail=newTail->next;
+         }
+         ListNode* newHead = newTail->next;
+         newTail->next=NULL;
+         return newHead;
+        
+    }
+};
