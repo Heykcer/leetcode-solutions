60f7f70964d19ce6729ffebcc02eabe88659585e
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-08-13 22:12:54 +0530

    Create 1297-MaximumNumberOfBalloons

---
+++ 1297-MaximumNumberOfBalloons/1297-MaximumNumberOfBalloons.java
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2026, 10:12:54 PM
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < text.length(); ++i) {
+            ++cnt[text.charAt(i) - 'a'];
+        }
+        cnt['l' - 'a'] >>= 1;
+        cnt['o' - 'a'] >>= 1;
+        int ans = 1 << 30;
+        for (char c : "balon".toCharArray()) {
+            ans = Math.min(ans, cnt[c - 'a']);
+        }
+        return ans;
+    }
+}
