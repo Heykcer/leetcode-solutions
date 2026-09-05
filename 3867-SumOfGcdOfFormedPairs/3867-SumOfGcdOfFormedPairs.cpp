155a39bd9d259b350bad7f589dbdc81c5f89b4c9
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 11:59:17 +0530

    Create 3867-SumOfGcdOfFormedPairs

---
+++ 3867-SumOfGcdOfFormedPairs/3867-SumOfGcdOfFormedPairs.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/5/2026, 11:59:16 AM
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix_gcd(n);
+        int mx = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            mx = max(mx, x);
+            prefix_gcd[i] = gcd(x, mx);
+        }
+
+        sort(prefix_gcd.begin(), prefix_gcd.end());
+
+        long long ans = 0;
+        for (int i = 0; i < n / 2; i++) {
+            ans += gcd(prefix_gcd[i], prefix_gcd[n - i - 1]);
+        }
+
+        return ans;
+    }
+};
