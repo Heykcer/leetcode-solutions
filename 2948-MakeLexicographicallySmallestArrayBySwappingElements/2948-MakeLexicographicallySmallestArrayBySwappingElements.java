0901165b8f9b71dc1eace4e2c7e93b78a3c30fed
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:02:20 +0530

    Create 2948-MakeLexicographicallySmallestArrayBySwappingElements

---
+++ 2948-MakeLexicographicallySmallestArrayBySwappingElements/2948-MakeLexicographicallySmallestArrayBySwappingElements.java
@@ -0,0 +1,24 @@
+// Last updated: 9/5/2026, 12:02:20 PM
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+         int n = nums.length;
+        Integer[] idx = new Integer[n];
+        Arrays.setAll(idx, i -> i);
+        Arrays.sort(idx, (i, j) -> nums[i] - nums[j]);
+        int[] ans = new int[n];
+        for (int i = 0; i < n;) {
+            int j = i + 1;
+            while (j < n && nums[idx[j]] - nums[idx[j - 1]] <= limit) {
+                ++j;
+            }
+            Integer[] t = Arrays.copyOfRange(idx, i, j);
+            Arrays.sort(t, (x, y) -> x - y);
+            for (int k = i; k < j; ++k) {
+                ans[t[k - i]] = nums[idx[k]];
+            }
+            i = j;
+        }
+        return ans;
+        
+    }
+}
