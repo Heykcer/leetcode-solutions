16501d3fdc635e11db1ee489ff5f35f1e5bbe64b
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:00:43 +0530

    Create 3536-MaximumProductOfTwoDigits

---
+++ 3536-MaximumProductOfTwoDigits/3536-MaximumProductOfTwoDigits.java
@@ -0,0 +1,16 @@
+// Last updated: 9/5/2026, 12:00:43 PM
+class Solution {
+    public int maxProduct(int n) {
+        int a = 0, b = 0;
+        for (; n > 0; n /= 10) {
+            int x = n % 10;
+            if (a < x) {
+                b = a;
+                a = x;
+            } else if (b < x) {
+                b = x;
+            }
+        }
+        return a * b;
+    }
+}
