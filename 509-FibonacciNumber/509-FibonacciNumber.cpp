8325de0cd069c1db4f5c4be127cd638dce14638f
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:05:19 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/5/2026, 12:05:19 PM
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)
+        return 0;
+        else if(n==1)
+        return 1;
+        else
+        return fib(n-1)+fib(n-2);
+        
+    }
+};
