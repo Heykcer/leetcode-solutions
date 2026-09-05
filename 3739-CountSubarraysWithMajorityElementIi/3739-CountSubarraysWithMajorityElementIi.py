e56b4a87f0c42d8f2be76c7d01f455a87dbc7967
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 11:59:44 +0530

    Create 3739-CountSubarraysWithMajorityElementIi

---
+++ 3739-CountSubarraysWithMajorityElementIi/3739-CountSubarraysWithMajorityElementIi.py
@@ -0,0 +1,47 @@
+# Last updated: 9/5/2026, 11:59:43 AM
+from bisect import bisect_left
+
+class BIT:
+    def __init__(self, n):
+        self.bit = [0] * (n + 1)
+
+    def update(self, i, val):
+        while i <= len(self.bit) - 1:
+            self.bit[i] += val
+            i += i & -i
+
+    def query(self, i):
+        s = 0
+        while i > 0:
+            s += self.bit[i]
+            i -= i & -i
+        return s
+
+
+class Solution(object):
+    def countMajoritySubarrays(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        prefix = [0]
+        s = 0
+
+        for x in nums:
+            if x == target:
+                s += 1
+            else:
+                s -= 1
+            prefix.append(s)
+
+        vals = sorted(set(prefix))
+        bit = BIT(len(vals))
+
+        ans = 0
+        for x in prefix:
+            idx = bisect_left(vals, x) + 1
+            ans += bit.query(idx - 1)
+            bit.update(idx, 1)
+
+        return ans
