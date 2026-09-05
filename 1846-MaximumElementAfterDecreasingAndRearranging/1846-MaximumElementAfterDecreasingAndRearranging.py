d81d719dad44f952bd130ac0207c3945f734e125
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:03:58 +0530

    Create 1846-MaximumElementAfterDecreasingAndRearranging

---
+++ 1846-MaximumElementAfterDecreasingAndRearranging/1846-MaximumElementAfterDecreasingAndRearranging.py
@@ -0,0 +1,15 @@
+# Last updated: 9/5/2026, 12:03:58 PM
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+
+        arr[0] = 1
+
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i], arr[i - 1] + 1)
+
+        return arr[-1]
