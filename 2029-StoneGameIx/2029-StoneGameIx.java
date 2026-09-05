c450acd942907fb42b800c9a39dc470fb69a065c
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:03:26 +0530

    Create 2029-StoneGameIx

---
+++ 2029-StoneGameIx/2029-StoneGameIx.java
@@ -0,0 +1,13 @@
+// Last updated: 9/5/2026, 12:03:25 PM
+class Solution {
+  public boolean stoneGameIX(int[] stones) {
+    int[] count = new int[3];
+
+    for (final int stone : stones)
+      ++count[stone % 3];
+
+    if (count[0] % 2 == 0)
+      return Math.min(count[1], count[2]) > 0;
+    return Math.abs(count[1] - count[2]) > 2;
+  }
+}
