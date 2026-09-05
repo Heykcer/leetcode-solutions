18e353c5cf7ba8ee17e1d4d7fa3665df7cc99915
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:03:11 +0530

    Create 2126-DestroyingAsteroids

---
+++ 2126-DestroyingAsteroids/2126-DestroyingAsteroids.cpp
@@ -0,0 +1,16 @@
+// Last updated: 9/5/2026, 12:03:10 PM
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long cm(mass);
+        sort(asteroids.begin(),asteroids.end());
+        for(int p=0;p<asteroids.size();p++)
+        {
+            if(cm<asteroids[p])
+            return false;
+            cm+=asteroids[p];
+        }
+        return true;
+        
+    }
+};
