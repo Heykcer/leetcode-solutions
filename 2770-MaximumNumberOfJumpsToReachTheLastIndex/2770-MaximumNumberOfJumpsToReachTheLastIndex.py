dd14388430d0f04ffe5e9f8c1538d3754d215975
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-05 12:02:35 +0530

    Create 2770-MaximumNumberOfJumpsToReachTheLastIndex

---
+++ 2770-MaximumNumberOfJumpsToReachTheLastIndex/2770-MaximumNumberOfJumpsToReachTheLastIndex.py
@@ -0,0 +1,12 @@
+# Last updated: 9/5/2026, 12:02:34 PM
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        dp[0]=0
+        for j in range(1,n):
+            for i in range(j):
+                if dp[i]!=-1 and abs(nums[j]-nums[i])<=target:
+                    dp[j]=max(dp[j],dp[i]+1)
+        return dp[n-1]
+        
