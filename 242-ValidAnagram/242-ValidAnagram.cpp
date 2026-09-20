7ba93766e15e15e658864f183e2e8664eb7d1f0c
Tanjil Alam <mdtanjilalam69@gmail.com>
2026-09-20 13:17:26 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/20/2026, 1:17:26 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     if(s.size()!=t.size())
+        return false;
+        int charCount[26]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            charCount[s[i]-'a']++;
+            charCount[t[i]-'a']--;
+        }
+        for(int count:charCount)
+        {
+            if(count!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+};
